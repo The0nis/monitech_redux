760f38c87dc9ded8480bbd1bb6be2f3e2ffb0f6d
TheOnis <onisamuelmichael@gmail.com>
2023-02-28 15:09:54 +0100

    code refactored

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
 import PostsList from "./features/posts/PostsList";
-import "./App.css";
 import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+import Layout from "./components/Layout";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">
-    <AddPostForm/>
-    <PostsList/>
-  </div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
